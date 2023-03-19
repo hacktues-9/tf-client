709efc5a565095ce1b433fda19e60b82e8c265e1
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-03-19 23:27:43 +0200

    [FIX] Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || "https://tuesfest.bg",
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [{
+                userAgent: "*",
+                allow: "/",
+            },
+            {
+                userAgent: "*",
+                disallow: "/api",
+            },
+            {
+                userAgent: "*",
+                disallow: "/admin",
+            },
+            {
+                userAgent: "*",
+                disallow: "/auth",
+            },
+        ],
+    },
+};
