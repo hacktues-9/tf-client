1d8c808e881bd4a886b1bf668398805f54f88f40
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-09 11:24:26 +0300

    [UPDATE] Front logo FONT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
 				id="front-section"
 				className="p-4 py-32 w-full min-h-screen flex flex-col lg:flex-row items-center justify-center gap-4 md:gap-16"
 			>
-				<h1 className="animate__animated animate__fadeInTopLeft !leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal drop-shadow-lg">
+				<h1 className="animate__animated animate__fadeInTopLeft !leading-none !tracking-wide text-8xl sm:text-8xl md:text-9xl text-center font-origin font-normal drop-shadow-lg">
 					TUES{' '}
-					<span className="!leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal bg-gradient text-transparent bg-clip-text drop-shadow-lg">
+					<span className="!leading-none !tracking-wide text-8xl sm:text-8xl md:text-9xl text-center font-origin font-normal bg-gradient text-transparent bg-clip-text drop-shadow-lg">
 						Fest
 					</span>
-					<br />
-					<span className="!leading-tight !tracking-wide text-6xl sm:text-5xl md:text-7xl text-center font-zone99 font-normal drop-shadow-lg">
+					<br className="text-3xl leading-none" />
+					<span className="!leading-none !tracking-wide text-7xl sm:text-7xl md:text-8xl text-center font-origin font-normal drop-shadow-lg">
 						2023
 					</span>
 				</h1>
