7100213e138a7a8ce3a5c2fc51714d7b20bf79d6
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-09 13:38:41 +0300

    [TRYFIX] Hydration animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,16 +15,18 @@ export default function Home() {
 				id="front-section"
 				className="p-4 py-32 w-full min-h-screen flex flex-col lg:flex-row items-center justify-center gap-4 md:gap-16"
 			>
-				<h1 className="animate__animated animate__fadeInTopLeft !leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal drop-shadow-lg">
-					TUES{' '}
-					<span className="!leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal bg-gradient text-transparent bg-clip-text drop-shadow-lg">
-						Fest
-					</span>
-					<br />
-					<span className="!leading-tight !tracking-wide text-6xl sm:text-5xl md:text-7xl text-center font-zone99 font-normal drop-shadow-lg">
-						2023
-					</span>
-				</h1>
+				<div className="animate__animated animate__fadeInTopLeft">
+					<h1 className="!leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal drop-shadow-lg">
+						TUES{' '}
+						<span className="!leading-tight !tracking-wide text-7xl sm:text-6xl md:text-8xl text-center font-zone99 font-normal bg-gradient text-transparent bg-clip-text drop-shadow-lg">
+							Fest
+						</span>
+						<br />
+						<span className="!leading-tight !tracking-wide text-6xl sm:text-5xl md:text-7xl text-center font-zone99 font-normal drop-shadow-lg">
+							2023
+						</span>
+					</h1>
+				</div>
 				<Countdown />
 			</section>
 			<About />
