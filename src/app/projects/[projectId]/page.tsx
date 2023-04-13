b32331ff7be1b9eb45644fa1a8d307172a03db90
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-13 14:06:06 +0300

    [UPDATE] Metadata titles

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,7 +1,60 @@
-const ProjectPage = () => {
-    <div>
-        <h1>Project Page</h1>
-    </div>
+export type Links = {
+	github: string;
+	demo: string;
+};
+
+export type Creator = {
+	name: string;
+	class: string;
+};
+
+export type Picture = {
+	url: string;
+	is_thumbnail: boolean;
+};
+
+export type Project = {
+	id: number;
+	name: string;
+	description: string;
+	video: string;
+	type: string;
+	category: string;
+	has_thumbnail: boolean;
+	links: Links;
+	creators: Creator[];
+	pictures: Picture[];
+};
+
+const getProject = async (id: string) => {
+	const res = await fetch(`https://api.tuesfest.bg/v1/get/project/${id}`, { cache: 'no-store' });
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error('Failed to fetch data');
+	}
+
+	const project: Project = await res.json();
+	console.warn(project);
+
+	return project;
+};
+
+export async function generateMetadata({
+	params,
+	searchParams,
+}: {
+	params: { projectId: string };
+	searchParams: URLSearchParams;
+}) {
+	const project = await getProject(params.projectId);
+	return { title: project.name };
 }
 
-export default ProjectPage
+const ProjectPage = () => {
+	<div>
+		<h1>Project Page</h1>
+	</div>;
+};
+
+export default ProjectPage;
--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,3 +1,12 @@
+export const metadata = {
+	title: {
+		default: 'Проекти',
+		template: '%s | ТУЕС Фест 2023',
+	},
+	description:
+		'Тук може да откриете проектите на учениците на ТУЕС. Тази година над 120 проекта ще бъдат представени само на 23 април в София Тех Парк - форум Джон Атанасов.',
+};
+
 const ProjectsLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="min-h-screen">
