e05bf8980ad562c3a80a2a26c56ca4a3b7deaa28
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-14 22:01:15 +0300

    [UPDATE] Projects card

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -27,14 +27,29 @@ export const metadata = {
 		'програмиране за деца',
 		'програмиране за ученици',
 	],
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Проекти | ТУЕС Фест 2023',
+		description:
+			'Тук може да откриете проектите на учениците на ТУЕС. Тази година над 120 проекта ще бъдат представени само на 23 април в София Тех Парк - форум Джон Атанасов.',
+		creator: '@tuesfest',
+		images: ['https://tuesfest.bg/logo/motto.png'],
+	},
+	assets: ['https://tuesfest.bg/favicon.png', 'https://tuesfest.bg/logo/motto.png', 'https://tuesfest.bg/assets'],
+	openGraph: {
+		title: 'Проекти | TUES Fest 2023',
+		description:
+			'Тук може да откриете проектите на учениците на ТУЕС. Тази година над 120 проекта ще бъдат представени само на 23 април в София Тех Парк - форум Джон Атанасов.',
+		url: `https://tuesfest.bg/projects`,
+		siteName: 'TUES Fest 2023',
+		images: ['https://tuesfest.bg/logo/motto.png'],
+		locale: 'bg-BG',
+		type: 'website',
+	},
 };
 
 const ProjectsLayout = ({ children }: { children: React.ReactNode }) => {
-	return (
-		<div className="min-h-screen">
-			{children}
-		</div>
-	);
+	return <div className="min-h-screen">{children}</div>;
 };
 
 export default ProjectsLayout;
