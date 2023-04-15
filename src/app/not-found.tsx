0b2db52eaead01caa47f2f2b8904d36ff4588a83
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-15 22:10:25 +0300

    [ADD] Not found pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+	return (
+		<div className="flex h-screen w-full flex-col items-center justify-center">
+			<h2 className="bg-gradient text-transparent bg-clip-text text-5xl font-black">
+				404 - Страницата не е намерена
+			</h2>
+		</div>
+	);
+}
