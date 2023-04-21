c86f754833a8c019e604d6d2f1fdd6ea57c0208a
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2023-04-21 11:53:37 +0300

    [ADD] Lazy Loading to Sponsors

--- src/partials/home/sponsors/Sponsor.tsx
+++ src/partials/home/sponsors/Sponsor.tsx
@@ -5,6 +5,7 @@ const Sponsor = ({ name, url, logo }: { name: string; url: string; logo: string 
 		<img
 			src={logo}
 			alt={name}
+			loading='lazy'
 			className="w-full sm:w-48 h-32 px-16 sm:px-4 py-8 object-contain !aspect-ratio-[12/8] bg-white rounded-xl hover:scale-105 transition-transform"
 		/>
 	</Link>
